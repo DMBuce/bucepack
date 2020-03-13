fad68c07ce6007a89f196d06e4159f252a9b3cd6
Buce <dmbuce@gmail.com>
2020-03-12 21:03:50 -0500

    Add script for generating blockstates/note_block.json

---
+++ bin/mk-note-block-blockstate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+template='		{
+			"when": { "OR": [
+				{ "note": "%s", "instrument": "%s" },
+				{ "note": "%s", "instrument": "%s" },
+				{ "note": "%s", "instrument": "%s" },
+				{ "note": "%s", "instrument": "%s" }
+			]},
+			"apply": { "model": "block/note_block_%s", "y": %s }
+		},
+'
+
+instrumentgroups=(
+	'%s bell %s chime %s cow_bell %s flute %s 0 %s'
+	'%s basedrum %s hat %s snare %s bass %s 90 %s'
+	'%s harp %s iron_xylophone %s xylophone %s guitar %s 180 %s'
+	'%s bit %s pling %s didgeridoo %s banjo %s 270 %s'
+)
+
+echo '{
+	"multipart": ['
+
+for note in {0..24}; do
+    for instgrp in "${instrumentgroups[@]}"; do
+        printf "$template" $(printf "$instgrp" $note $note $note $note $note)
+	done
+done | sed '$ s/,$//'
+
+echo '	]
+}'
+
