40b68d47aed88f501f53386c8ae03fb1f3900eff
Buce <dmbuce@gmail.com>
2021-07-10 19:21:37 -0500

    Tweak artifacts

--- data/minecraft/loot_tables/artifact.pl
+++ data/minecraft/loot_tables/artifact.pl
@@ -2,13 +2,13 @@
 
 use File::Basename;
 
-print dirname($0)."/subtables/artifact.json.yaml\n";
+#print dirname($0)."/subtables/artifact.json.yaml\n";
 open(FH, '<', dirname($0)."/subtables/artifact.json.yaml") or die $!;
 while (<FH>) {
-	if (!m/^#/ && m/Name:..........................([^\\]*)/) {
+	if (!m/^#/ && m/Name:.*text\\\\\\":\\\\\\"([^\\]*)/) {
 		print $1;
-		if (m/Lore:.......([^\\]*)/) {
-			 print ": ", $1, "\n"
+		if (m/Lore:.*§.([^\\]*)/) {
+			 print ", ", $1, "\n"
 		} else {
 			print "\n"
 		}
