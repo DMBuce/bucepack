a0b86ced9189c60e55c875bcfc2460a3001a7dab
Buce <dmbuce@gmail.com>
2021-11-10 17:14:36 -0600

    Update chk-weight script

--- bin/chk-weight.sh
+++ bin/chk-weight.sh
@@ -2,6 +2,7 @@
 
 find data/minecraft/loot_tables/{chests,gameplay,orig/chests,orig/gameplay} -name \*.yaml \
 | while read file; do
-	echo "$(grep -o "weight: *[0-9]*" "$file" \
+	echo "$(grep -v -e relic/random -e 'type: empty.*weight: [0-9]*9' "$file" \
+	| grep -o "weight: *[0-9]*" \
 	| awk '{sum += $2} END {print sum}') $file"
-done | sed 's/orig\///' | sort | uniq -u
+done | sed 's/orig\///' | sort | uniq -u | sort -k2
