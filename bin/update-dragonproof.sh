73c69a14c90a585ac39ca54d198efa343fb3be8d
Buce <dmbuce@gmail.com>
2022-05-09 17:24:43 -0500

    Simplify update-dragonproof.sh
    Use a sempl template to do the heavy lifting.

--- bin/update-dragonproof.sh
+++ bin/update-dragonproof.sh
@@ -1,35 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd "$(git rev-parse --show-toplevel)"
 
-latest="$(./bin/latest)"
-
-exec >data/minecraft/tags/blocks/dragon_immune.json
-
-echo '{
-  "AUTO-GENERATED-BY": "'"${0##*/}"'",
-  "replace": false,
-  "values": [
-    "minecraft:end_stone_bricks",
-    "minecraft:end_stone_brick_slab",
-    "minecraft:end_stone_brick_stairs",
-    "minecraft:end_stone_brick_wall",
-    "minecraft:purpur_block",
-    "minecraft:purpur_pillar",
-    "minecraft:purpur_slab",
-    "minecraft:purpur_stairs",
-    "minecraft:chorus_plant",
-    "minecraft:chorus_flower",
-    "minecraft:end_rod",
-    "minecraft:ender_chest",
-    "#minecraft:shulker_boxes",
-    "minecraft:dragon_egg",
-    "minecraft:dragon_head",'
-
-printf "%s\n" "$latest.jar/assets/minecraft/blockstates/"* \
-	| sed -n '/magenta/ { s/.*\///; s/\..*$//; p; }' \
-	| sed 's/.*/    "minecraft:&",/; $s/,$//'
-
-echo '  ]
-}'
+touch data/minecraft/tags/blocks/dragon_immune.json.sempl
+make data/minecraft/tags/blocks/dragon_immune.json
 
