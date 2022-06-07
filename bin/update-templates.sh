9201479ae729d855bad657fc3136486a1dd8b2ca
Buce <dmbuce@gmail.com>
2022-06-07 16:48:58 -0500

    Update Path Strider blocks for 1.19

---
+++ bin/update-templates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update timestamp of files that generate from minecraft assets
+touch \
+	data/minecraft/tags/blocks/dragon_immune.json.sempl \
+	data/minecraft/tags/blocks/soul_speed_blocks.json.sempl \
+
