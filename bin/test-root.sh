bfdc7a635ac26ac31aa401ebf721e20927661140
Buce <dmbuce@gmail.com>
2023-02-19 22:28:20 -0600

    Tweak test-root.sh

--- bin/test-root.sh
+++ bin/test-root.sh
@@ -12,7 +12,7 @@ for file in $packfiles; do
 	pack="${file%.zip}"
 	grep -qx "$pack" "cache/skip.packs" && continue
 	grep -v /tags/ "cache/$pack.files" \
-		| grep -q -e/{functions,predicates,worldgen}/ \
+		| grep -q -e/{functions,predicates}/ \
 		|| continue
 
 	if ! grep -qx 'data/buce/advancements/root.json' "cache/$pack.files"; then
@@ -27,7 +27,15 @@ for file in $packfiles; do
 done
 
 echo "Checking datapack advancement icons..."
-if find buce-data/ -name \*.advancement.json \! -path \*deleteme\* -exec jq -r .display.icon.item {} + | sort | uniq -dc | sort -hr | grep -v 'minecraft:barrier' | grep .; then
+if find buce-data/ \
+	-name \*.advancement.json \
+	\! -path \*deleteme\* \
+	\! -path \*disabled\* \
+	-exec jq -r .display.icon.item {} + \
+	| sort | uniq -dc | sort -hr \
+	| grep -v 'minecraft:barrier' \
+	| grep .
+then
 	retval=1
 	echo "> Duplicate advancement icons found, see above"
 fi
