51b2715da73c927b1656ea41a458bc0397092d9f
Buce <dmbuce@gmail.com>
2023-04-15 18:44:24 -0500

    Improve documentation

---
+++ bin/test-docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# redirect all output to stderr
+exec 1>&2
+
+packfiles="$(echo ${PACKFILES-$(make list)})"
+retval=0
+
+echo 'Checking that all documented packs exist...' >&2
+for pack in $(sed -n '/packsummary/ {
+	s/.*packsummary //
+	s/}.*//
+	p
+}' README.asciidoc.sempl); do
+	if [[ ! $packfiles =~ (^| )$pack.zip( |$) ]]; then
+		echo "> Nonexistent pack is documented: $pack"
+		retval=1
+	fi
+done
+
+echo 'Checking that all packs are documented...' >&2
+for file in $packfiles; do
+	if ! grep -q "\b${i%.zip}\b" README.asciidoc.sempl; then
+		echo "> Missing documentation for pack: $pack"
+		retval=1
+	fi
+done
+
+exit $retval
+
